762a2f8330bece33e37b0a81f08dfefe945bfbf2
rjl888 <146054914+rjl888@users.noreply.github.com>
2024-08-07 17:38:35 +0800

    Update error.go

--- service/error.go
+++ service/error.go
@@ -3,7 +3,6 @@ package service
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"one-api/common"
 	"one-api/dto"
@@ -31,13 +30,13 @@ func OpenAIErrorWrapper(err error, code string, statusCode int) *dto.OpenAIError
 	// 定义一个正则表达式匹配URL
 	if strings.Contains(text, "Post") || strings.Contains(text, "dial") {
 		common.SysLog(fmt.Sprintf("error: %s", text))
-		text = "请求上游地址失败"
+		text = "请求失败"
 	}
 	//避免暴露内部错误
 
 	openAIError := dto.OpenAIError{
 		Message: text,
-		Type:    "new_api_error",
+		Type:    "rjl_api_error",
 		Code:    code,
 	}
 	return &dto.OpenAIErrorWithStatusCode{
@@ -56,32 +55,10 @@ func RelayErrorHandler(resp *http.Response) (errWithStatusCode *dto.OpenAIErrorW
 	errWithStatusCode = &dto.OpenAIErrorWithStatusCode{
 		StatusCode: resp.StatusCode,
 		Error: dto.OpenAIError{
-			Type:  "upstream_error",
-			Code:  "bad_response_status_code",
-			Param: strconv.Itoa(resp.StatusCode),
+			Message: "请稍后再试试，如长时间不可用，请通知管理员q2411728460 进行处理",
+			Type:    "model_unavailable",
+			Code:    "model_unavailable",
 		},
-	}
-	responseBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return
-	}
-	err = resp.Body.Close()
-	if err != nil {
-		return
-	}
-	var errResponse dto.GeneralErrorResponse
-	err = json.Unmarshal(responseBody, &errResponse)
-	if err != nil {
-		return
-	}
-	if errResponse.Error.Message != "" {
-		// OpenAI format error, so we override the default one
-		errWithStatusCode.Error = errResponse.Error
-	} else {
-		errWithStatusCode.Error.Message = errResponse.ToMessage()
-	}
-	if errWithStatusCode.Error.Message == "" {
-		errWithStatusCode.Error.Message = fmt.Sprintf("bad response status code %d", resp.StatusCode)
 	}
 	return
 }
@@ -117,7 +94,7 @@ func TaskErrorWrapper(err error, code string, statusCode int) *dto.TaskError {
 	// 定义一个正则表达式匹配URL
 	if strings.Contains(text, "Post") || strings.Contains(text, "dial") {
 		common.SysLog(fmt.Sprintf("error: %s", text))
-		text = "请求上游地址失败"
+		text = "请求失败"
 	}
 	//避免暴露内部错误
 
